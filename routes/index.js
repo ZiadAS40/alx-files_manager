056c2baee38051b3c477cd66649ad307e1ff5ba1
ZiadAS40 <ziad403612@gmail.com>
2025-04-17 13:48:31 +0200

    add tests file and replace imports with ES6 ones

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const AppController = require('../controllers/AppController');
+import express from 'express';
+import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
