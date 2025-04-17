129e9f8b954e4532ce85072049c1b54e7eacad57
ZiadAS40 <ziad403612@gmail.com>
2025-04-17 14:24:16 +0200

    fixing retured format

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,11 +4,6 @@ import dbClient from '../utils/db';
 const getStatus = (req, res) => {
   if (redisClient.isAlive() && dbClient.isAlive()) {
     res.status(200).json({ redis: true, db: true });
-  } else {
-    res.status(500).json({
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    });
   }
 };
 
